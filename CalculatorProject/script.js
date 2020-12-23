a7f8bcc079078233986fe8aa072fbfe7495f9509
Alp Niksarlı <72332955+alpnix@users.noreply.github.com>
2020-12-23 15:49:01 +0300

    easier to read

--- CalculatorProject/script.js
+++ CalculatorProject/script.js
@@ -1,13 +1,21 @@
 // defining constant nodes and node lists
+
 const display = document.querySelector(".display");
+
 const clear = document.querySelector("#clear");
+
 const dot = document.querySelector("#dot");
+
 const equal = document.querySelector("#equal");
+
 const operators = document.querySelectorAll(".row-4 button");
+
 const number_keys = document.querySelectorAll(".keys .number");
 
 // looping through operators
-for (const operator of operators){
+
+for (const operator of operators)
+{
     operator.addEventListener("click",function(){
         if (display.innerHTML == 0 || display.innerHTML == "Infinity" || display.innerHTML == "Error")
         {
@@ -21,12 +29,14 @@ for (const operator of operators){
 }
 
 // displaying number passing function
-for (const number of number_keys){
+for (const number of number_keys)
+{
     number.addEventListener("click",putonScreen);
 }
 
 // displaying number function
-function putonScreen(){
+function putonScreen()
+{
     if (display.innerHTML == 0 || display.innerHTML == "Infinity" || display.innerHTML == "Error")
     {
         display.innerHTML = this.innerHTML;
@@ -41,12 +51,14 @@ function putonScreen(){
 clear.addEventListener("click",function(){
     display.innerHTML = 0;
 })
+
 dot.addEventListener("click",function(){
     display.innerHTML += this.innerHTML
 })
 
 /*evaluating the screen and
 printing the result on the screen*/ 
+
 equal.addEventListener("click",function(){
     try
     {
